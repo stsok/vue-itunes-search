34004b679f8674afa38cb587ae50228f9946917a
stsok <30542262+stsok@users.noreply.github.com>
2018-10-14 13:58:42 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="root">
+    <Search class="search" @loadStart="onLoadStart" @loadComplete="onLoadComplete"/>
+    <Result :items="items" :loadProgress="loadProgress"/>
+  </div>
+</template>
+
+<script>
+import Search from "@/components/Search";
+import Result from "@/components/Result";
+
+export default {
+  data() {
+    return {
+      items: [],
+      loadProgress: false,
+    };
+  },
+  methods: {
+    onLoadStart() {
+      this.loadProgress = true;
+    },
+    onLoadComplete({ results }) {
+      this.items = results;
+      this.loadProgress = false;
+    },
+  },
+  components: {
+    Search,
+    Result,
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  padding-top: 70px;
+}
+
+.search {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+</style>
+
+
+<style>
+html {
+  font-size: 62.5%;
+}
+
+body {
+  margin: 0;
+  color: #35495e;
+}
+
+p {
+  margin: 0;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="container">
+      <input class="text" type="text" v-model="term" @keyup.enter="exe">
+      <input class="submit" type="submit" value="Search" @click="exe">
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      term: "",
+    }
+  },
+  methods: {
+    async exe() {
+      this.$emit("loadStart")
+      const { data } = await axios.get(`//itunes.apple.com/search?term=${this.term}&country=jp&entity=musicVideo`);
+      this.$emit("loadComplete", { results: data.results })
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  height: 70px;
+  padding: 20px;
+  background-color: #35495e;
+  box-sizing: border-box;
+}
+
+.text {
+  width: 50%;
+  max-width: 300px;
+  padding: .5em;
+  border: none;
+}
+
+.submit {
+  padding: .5em 2em;
+  margin-left: 10px;
+  color: #fff;
+  background-color: #42b883;
+  border: none;
+  border-radius: 20px;
+}
+</style>
